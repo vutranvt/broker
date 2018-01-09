337d590d0f0e9c628c14bf600fb9b086461632f8
vutranvt <vutranvt@gmail.com>
2018-01-09 09:26:08 +0700

    change sql

--- broker.js
+++ broker.js
@@ -21,29 +21,17 @@ const config = {
     user: 'sa',
     password: '123',
     server: 'localhost\\VSQL', // You can use 'localhost\\instance' to connect to named instance
-    database: 'testdb',
+    database: 'iot',
 
     options: {
         encrypt: true // Use this if you're on Windows Azure
     }
 }
 
-
-const pool1 = new sql.ConnectionPool(config);
-const pool2 = new sql.ConnectionPool(config);
-
-var temp = 'nth/test';
-
-pool2.connect(err => {
-    // ...
-    pool2.request().query(`insert into DATA (Topic, Value1, Value2, Value3) values (\'${temp}\', 31, 31, 31)` , (err, result) => {
-        // ... error checks
-
-        // console.dir(result)
-        console.dir(result);
-        // console.dir(result.recordset[0].Value1);
-    })
-})
+const poolConnected = new sql.ConnectionPool(config);
+const poolData = new sql.ConnectionPool(config);
+const poolClient = new sql.ConnectionPool(config);
+const poolConfig = new sql.ConnectionPool(config);
 
 var authenticate = function (client, username, password, callback) {
     if (username == "esp32" && password.toString() == "mtt@23377")
@@ -80,6 +68,7 @@ function setup() {
     broker.authorizePublish = authorizePublish;
     broker.authorizeSubscribe = authorizeSubscribe;
 
+
     console.log('Mosca broker is up and running')
 }
 
@@ -88,6 +77,20 @@ function setup() {
 broker.on('clientConnected', function (client) {
     console.log('Client connected: ', client.id);
     console.log('----------------');
+    var now = Date();
+    poolConnected.connect(err => {
+        // ...
+        
+        poolConnected.request().query(`insert into CLIENT (ClientId) 
+            values (\'${client.id}\')`, (err, result) => {
+                // ... error checks
+                console.log('----------------');
+                // console.dir(result)
+                console.dir(result);
+                // console.dir(result.recordset[0].Value1);
+            })
+    })
+
 
 });
 
@@ -98,22 +101,25 @@ broker.on('published', function (packet, client) {
     var stringTopic = packet.topic.toString('utf-8');
     var objPayload = stringPayload;
 
-    // try {
-    //     //Nếu Payload là chuối JSON thì convert to Object
-    //     objPayload = JSON.parse(stringPayload);
-    // } catch (err) {
-    //     //Xuất lỗi nếu Payload ko phải là chuổi JSON
-    //     console.log(err.message);
-    // }
+    try {
+        //Nếu Payload là chuối JSON thì convert to Object
+        objPayload = JSON.parse(stringPayload);
+    } catch (err) {
+        //Xuất lỗi nếu Payload ko phải là chuổi JSON
+        console.log(err.message);
+    }
     console.log('Published data: ', objPayload);
-    console.log('Published data: ', stringPayload);
+    // console.log('Published data: ', client.id);        
     console.log('--------------- ');
 
-    var data = 'nth/test';
-
-        pool1.connect(err => {
+    if (objPayload.value1!=undefined && client.id!=undefined) {
+        console.log('--------------- ');
+        console.log('Client published: ', client.id);
+        poolData.connect(err => {
             // ...
-            pool1.request().query(`insert into DATA (Topic, Value1, Value2, Value3) values (\'${data}\', 32, 32, 32)`, (err, result) => {
+            
+            poolData.request().query(`insert into CURRENT_DATA (ClientId, Value1, Value2, Value3, Topic) 
+            values (\'${client.id}\', \'${objPayload.value1}\', \'${objPayload.value2}\', \'${objPayload.value3}\', \'${packet.topic}\')`, (err, result) => {
                 // ... error checks
 
                 // console.dir(result)
@@ -121,9 +127,52 @@ broker.on('published', function (packet, client) {
                 // console.dir(result.recordset[0].Value1);
             })
         })
+    } else if (objPayload.type != undefined) {
+        console.log('--------------- ');
+        console.log('--------------- ');
+
+        poolClient.connect(err=> {
+            poolClient.request().query(`UPDATE CLIENT
+            SET Location = \'${objPayload.location}\'
+                ,Type = \'${objPayload.type}\'
+                ,Name = \'${objPayload.name}\'
+                ,Mac = \'${objPayload.mac}\'
+                ,Server = \'${objPayload.server}\'
+                ,Port = \'${objPayload.port}\'
+                ,Version = \'${objPayload.version}\'
+                ,UpdateFile = \'${objPayload.updateFile}\'
+            WHERE ClientId = \'${client.id}\'`, (err, result) => {
+                    // ... error checks
+
+                    // console.dir(result)
+                    console.dir(result);
+                    // console.dir(result.recordset[0].Value1);
+            })
+        }) 
+        //add "current" app
+        if (objPayload.type === "current") {        
+            console.log('current--------------- ');
+            poolConfig.connect(err => {
+                poolConfig.request().query(`insert into CURRENT_CONFIG (ClientId, Calib, TI, AdcZero, AdcPin) 
+                values (\'${client.id}\'
+                    , \'${objPayload.config.calib}\'
+                    , \'${objPayload.config.TI}\'
+                    , \'${objPayload.config.adcZero}\'
+                    , \'${objPayload.config.adcPin}\')`, (err, result) => {
+                        // ... error checks
+
+                        // console.dir(result)
+                        console.dir(result);
+                        // console.dir(result.recordset[0].Value1);
+                })
+            })
+        }
+        //add other app
+        else if (objPayload.type === "huan") {  
+
+        }
+    }
     
-
-
 });
 
 // fired when a client subscribes to a topic
